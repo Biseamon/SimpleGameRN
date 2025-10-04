6e16bc1026acc70a266720605b14a2979519be9d
MarKa <capranov922@yahoo.com>
2025-10-04 13:53:42 +0100

    status bar

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StyleSheet, ImageBackground } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { LinearGradient } from 'expo-linear-gradient';
-import StartGameScreen from './screens/StartGameScreen';
 import { useState,useEffect } from 'react';
 import { useFonts } from 'expo-font';
+import { StatusBar } from 'expo-status-bar';
 
 
 import GameScreen from './screens/GameScreen';
 import GameOverScreen from './screens/GameOverScreen';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
 
@@ -56,16 +57,22 @@ export default function App() {
   }
 
   return (
-    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
-      <ImageBackground 
-        source={require('./assets/images/background.png')} 
-        resizeMode="cover"
+    <>
+    <StatusBar style="light" />
+      <LinearGradient 
+        colors={['#4e0329', '#ddb52f']} 
         style={styles.rootScreen}
-        imageStyle={styles.backgroundImage}
-        >
-          <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
-      </ImageBackground>   
-    </LinearGradient>
+      >
+        <ImageBackground 
+          source={require('./assets/images/background.png')} 
+          resizeMode="cover"
+          style={styles.rootScreen}
+          imageStyle={styles.backgroundImage}
+          >
+            <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+        </ImageBackground>   
+      </LinearGradient>
+    </>
   );
 }
 
